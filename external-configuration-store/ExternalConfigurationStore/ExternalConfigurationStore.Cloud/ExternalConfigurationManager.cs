96da5d96b6b0c43732ae9c82b0259d4c59779887
Federico Arambarri <far@clariusconsulting.net>
2016-05-23 12:05:18 -0300

    External Configuration Store Migration

--- external-configuration-store/ExternalConfigurationStore/ExternalConfigurationStore.Cloud/ExternalConfigurationManager.cs
+++ external-configuration-store/ExternalConfigurationStore/ExternalConfigurationStore.Cloud/ExternalConfigurationManager.cs
@@ -1,14 +1,5 @@
-﻿// ==============================================================================================================
-// Microsoft patterns & practices
-// Cloud Design Patterns project
-// ==============================================================================================================
-// ©2013 Microsoft. All rights reserved. 
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance 
-// with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software distributed under the License is 
-// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and limitations under the License.
-// ==============================================================================================================
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 namespace ExternalConfigurationStore.Cloud
 {
     using System;
@@ -193,15 +184,17 @@ namespace ExternalConfigurationStore.Cloud
                 {
                     this.settingsCacheLock.EnterWriteLock();
 
+                    if (this.settingsCache != null)
+                    {
+                        //Notify settings changed
+                        latestSettings.Except(this.settingsCache).ToList().ForEach(kv => this.changed.OnNext(kv));
+                    }
                     this.settingsCache = latestSettings;
                 }
                 finally
                 {
                     this.settingsCacheLock.ExitWriteLock();
                 }
-
-                //Notify settings changed
-                latestSettings.Except(this.settingsCache).ToList().ForEach(kv => this.changed.OnNext(kv));
 
                 // Update the current version.
                 this.currentVersion = latestVersion;
--- external-configuration-store/ExternalConfigurationStore/ExternalConfigurationStore.Cloud/WorkerRole.cs
+++ external-configuration-store/ExternalConfigurationStore/ExternalConfigurationStore.Cloud/WorkerRole.cs
@@ -1,21 +1,12 @@
-﻿// ==============================================================================================================
-// Microsoft patterns & practices
-// Cloud Design Patterns project
-// ==============================================================================================================
-// ©2013 Microsoft. All rights reserved. 
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance 
-// with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software distributed under the License is 
-// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and limitations under the License.
-// ==============================================================================================================
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 namespace ExternalConfigurationStore.Cloud
 {
     using System;
     using System.Diagnostics;
     using System.Net;
     using System.Threading;
-    using Microsoft.WindowsAzure;
+    using Microsoft.Azure;
     using Microsoft.WindowsAzure.ServiceRuntime;
     using Microsoft.WindowsAzure.Storage;
 
@@ -82,10 +73,10 @@ namespace ExternalConfigurationStore.Cloud
             container.CreateIfNotExists();
 
             var productionBlob = container.GetBlockBlobReference(this.configBlobNameProduction);
-            productionBlob.UploadFromFile(this.configBlobNameProduction, System.IO.FileMode.Open);
+            productionBlob.UploadFromFile(this.configBlobNameProduction);
 
             var stagingBlob = container.GetBlockBlobReference(this.configBlobNameStaging);
-            stagingBlob.UploadFromFile(this.configBlobNameStaging, System.IO.FileMode.Open);
+            stagingBlob.UploadFromFile(this.configBlobNameStaging);
         }
 
         /// <summary>
