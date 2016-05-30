a0844ee6d6ed8cd818544b206945173637d0a4d7
Federico Arambarri <far@clariusconsulting.net>
2016-05-30 15:30:27 -0300

    Migration health-endpoint-monitoring

--- health-endpoint-monitoring/HealthEndPointMonitoring.Web/Controllers/HealthCheckController.cs
+++ health-endpoint-monitoring/HealthEndPointMonitoring.Web/Controllers/HealthCheckController.cs
@@ -1,21 +1,12 @@
-﻿// ==============================================================================================================
-// Microsoft patterns & practices
-// Cloud Design Patterns project
-// ==============================================================================================================
-// ©2013 Microsoft. All rights reserved. 
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance 
-// with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software distributed under the License is 
-// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and limitations under the License.
-// ==============================================================================================================
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 namespace HealthEndPointMonitoring.Web.Controllers
 {
     using System;
     using System.Diagnostics;
     using System.Net;
     using System.Web.Mvc;
-    using Microsoft.WindowsAzure;
+    using Microsoft.Azure;
 
     public class HealthCheckController : Controller
     {
@@ -74,7 +65,7 @@ namespace HealthEndPointMonitoring.Web.Controllers
         /// <summary>
         /// Test check that randomly returns 500 or 200 status codes
         /// emulating an unstable service.
-        /// /HealthCheck/TestRandomResponse/
+        /// /HealthCheck/CheckUnstableServiceHealth 
         /// </summary>
         /// <returns></returns>
         public ActionResult CheckUnstableServiceHealth()
@@ -88,7 +79,7 @@ namespace HealthEndPointMonitoring.Web.Controllers
 
         /// <summary>
         /// Test health check that returns a response code set in configuration for testing
-        /// /HealthCheck/TestResponseFromConfig
+        /// /HealthCheck/TestResponseFromConfig 
         /// </summary>
         /// <returns></returns>
         public ActionResult TestResponseFromConfig()
