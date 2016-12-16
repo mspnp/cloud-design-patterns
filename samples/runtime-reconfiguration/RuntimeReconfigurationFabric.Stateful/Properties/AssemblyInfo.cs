10c1ce73823f1c1cebb2063229eb14e2b5075f20
luisgizirian <v-lugizi@microsoft.com>
2016-12-16 14:24:06 -0300

    Runtime Reconfiguration sample based on Stateful Reliable Service.
    Signed-off-by: luisgizirian <v-lugizi@microsoft.com>

--- samples/runtime-reconfiguration/RuntimeReconfigurationFabric.Stateful/Properties/AssemblyInfo.cs
+++ samples/runtime-reconfiguration/RuntimeReconfigurationFabric.Stateful/Properties/AssemblyInfo.cs
@@ -1,18 +1,16 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RuntimeReconfiguration.Web")]
+[assembly: AssemblyTitle("RuntimeReconfigurationFabric.Stateful")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RuntimeReconfiguration.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("RuntimeReconfigurationFabric.Stateful")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("69928926-1ec4-4d9f-810d-3f80320d65e9")]
+[assembly: Guid("5d5a1d8e-61ad-46b6-ad1b-e0f51a5d4877")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
