97953bfc08c2acd7d4dab692a68212ae1839df36
Pedro Wood <mail_pedro@yahoo.com.ar>
2017-05-12 13:48:37 -0300

    review's requested changes

--- leader-election-sf/WorkerStatelessService/Program.cs
+++ leader-election-sf/WorkerStatelessService/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Fabric;
 using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.ServiceFabric.Services.Runtime;
 
 namespace WorkerStatelessService
--- leader-election-sf/WorkerStatelessService/WorkerStatelessService.cs
+++ leader-election-sf/WorkerStatelessService/WorkerStatelessService.cs
@@ -6,6 +6,8 @@ using Microsoft.ServiceFabric.Services.Runtime;
 using Shared.Service;
 using Microsoft.ServiceFabric.Services.Remoting.Client;
 using Microsoft.ServiceFabric.Services.Client;
+using System.Configuration;
+using System.Linq;
 
 namespace WorkerStatelessService
 {
@@ -14,6 +16,10 @@ namespace WorkerStatelessService
     /// </summary>
     internal sealed class WorkerStatelessService : StatelessService
     {
+        private static readonly string LeaderServiceName = ConfigurationManager.AppSettings["LeaderServiceName"];
+        private static readonly int secondsBetweenRetries = 10;
+        private static readonly int numberOfRetries = 12;
+
         public WorkerStatelessService(StatelessServiceContext context)
             : base(context)
         { }
@@ -24,23 +30,17 @@ namespace WorkerStatelessService
         /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
         protected override async Task RunAsync(CancellationToken cancellationToken)
         {
+            var serviceUri = new Uri("fabric:" + LeaderServiceName);
+            await WaitForLeader(LeaderServiceName, cancellationToken);
+
             // TODO: Replace the following sample code with your own logic 
             //       or remove this RunAsync override if it's not needed in your service.
-
-            // NOTE: Waits to "ensure" Leader starting first. Not realy needed.
-            //       Not sure if there's a better way (i.e. service dependency enforcement into SF).
-            //       Altenatives in SO answer here:
-            //       http://stackoverflow.com/questions/41370742/azure-service-fabric-specify-service-application-startup-dependency
-            await Task.Delay(20000, cancellationToken);
 
             while (true)
             {
                 cancellationToken.ThrowIfCancellationRequested();
 
-                // NOTE: move this to Service Config
-                var serviceUri = new Uri("fabric:/LeaderElectionSFWay/LeaderStatefulService");
-
-                ILeaderService service = ServiceProxy.Create<ILeaderService>(serviceUri, new ServicePartitionKey(1));
+                var service = ServiceProxy.Create<ILeaderService>(serviceUri, new ServicePartitionKey(1));
 
                 var load = await service.GetWorkloadChunk();
 
@@ -51,6 +51,36 @@ namespace WorkerStatelessService
 
                 await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
             }
+        }
+
+        private static async Task WaitForLeader(string leaderServiceName, CancellationToken cancellationToken, int count = 1)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if(count == numberOfRetries)
+                throw new TimeoutException("Leader not initialized after " + count * secondsBetweenRetries + " seconds" );
+
+            var fabricClient = new FabricClient();
+            var apps = fabricClient.QueryManager.GetApplicationListAsync().Result;
+            
+            if (apps.Any())
+            {
+                var services = await fabricClient.QueryManager.GetServiceListAsync(apps.First().ApplicationName);
+                if (services.All(s => s.ServiceName.LocalPath != leaderServiceName))
+                {
+                    await Wait(cancellationToken, count);
+                }
+            }
+            else
+            {
+                await Wait(cancellationToken, count);
+            }
+        }
+
+        private static async Task Wait(CancellationToken cancellationToken, int count)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(secondsBetweenRetries*count), cancellationToken);
+            count++;
+            await WaitForLeader(LeaderServiceName, cancellationToken, count);
         }
     }
 }
