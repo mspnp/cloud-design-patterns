8cdeda552419185d81e7b7a4de0fe5726598f980
Luyunmt <2534523917@qq.com>
2020-02-03 11:50:56 +0800

    deal with conflict

--- claim-check/code-samples/sample-2/client-consumer/EventReader.cs
+++ claim-check/code-samples/sample-2/client-consumer/EventReader.cs
@@ -76,7 +76,7 @@ namespace ClientConsumer
                 try
                 {                  
                     string body = Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray());
-                    JToken jsonMessage = JArray.Parse(body).First;
+                    var jsonMessage = JArray.Parse(body).First;
                     Uri uploadedUri = new Uri(jsonMessage["data"]["url"].ToString());
                     Console.WriteLine("Blob available at: {0}", uploadedUri);
                     BlockBlobClient blockBlob = new BlockBlobClient(uploadedUri);
