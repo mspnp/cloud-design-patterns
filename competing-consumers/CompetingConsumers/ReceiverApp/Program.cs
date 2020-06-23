e333c63129e6f8c391babec072112d61a86630aa
magrande <v-magran@microsoft.com>
2020-06-22 23:52:09 -0300

    Update competing consumers pattern to use App Service Web Jobs

---
+++ competing-consumers/CompetingConsumers/ReceiverApp/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace ReceiverApp
+{
+ using Microsoft.Azure.ServiceBus;
+ using System;
+ using System.Configuration;
+ using System.Text;
+ using System.Threading;
+ using System.Threading.Tasks;
+
+ class Program
+ {
+  // Connection String for the namespace can be obtained from the Azure portal under the 
+  // 'Shared Access policies' section.
+  private static readonly string queueName = ConfigurationManager.AppSettings.Get("QueueName");
+  private static readonly string serviceBusConnectionString = ConfigurationManager.AppSettings.Get("ServiceBusConnectionString");
+  static IQueueClient queueClient;
+  private static bool keepRunning = true;
+
+  static void Main(string[] args)
+  {
+   AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
+
+   MainAsync().GetAwaiter().GetResult();
+  }
+
+  static async Task MainAsync()
+  {
+   // Register QueueClient's MessageHandler and receive messages in a loop
+   RegisterOnMessageHandlerAndReceiveMessages();
+
+   while (keepRunning);
+  }
+
+  static void RegisterOnMessageHandlerAndReceiveMessages()
+  {
+   queueClient = new QueueClient(serviceBusConnectionString, queueName);
+
+   Console.WriteLine("Initializing receiver App");
+
+   var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+   {
+    MaxConcurrentCalls = 1,
+    AutoComplete = false
+   };
+
+   queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+  }
+
+  static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+  {
+   // Process the message
+   Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+   if (!token.IsCancellationRequested)
+   {
+    await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+   }
+
+  }
+
+  static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+  {
+   Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+   var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+   Console.WriteLine("Exception context for troubleshooting:");
+   Console.WriteLine($"- Endpoint: {context.Endpoint}");
+   Console.WriteLine($"- Entity Path: {context.EntityPath}");
+   Console.WriteLine($"- Executing Action: {context.Action}");
+   return Task.CompletedTask;
+  }
+  static async void CurrentDomain_ProcessExit(object sender, EventArgs e)
+  {
+   await queueClient.CloseAsync();
+   Console.WriteLine("Exiting receiver app");
+  }
+ }
+}
---
+++ competing-consumers/CompetingConsumers/SenderApp/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace SenderApp
+{
+ using Microsoft.Azure.ServiceBus;
+ using System;
+ using System.Configuration;
+ using System.Text;
+ using System.Threading;
+ using System.Threading.Tasks;
+
+ class Program
+ {
+  // Connection String for the namespace can be obtained from the Azure portal under the 
+  // 'Shared Access policies' section.
+
+  private static readonly string queueName = ConfigurationManager.AppSettings.Get("QueueName");
+  private static readonly string serviceBusConnectionString = ConfigurationManager.AppSettings.Get("ServiceBusConnectionString");
+  private static bool keepRunning = true;
+  private static int msgCounter = 0;
+  static IQueueClient queueClient;
+
+  public static async Task Main(string[] args)
+  {
+
+   AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
+
+   queueClient = new QueueClient(serviceBusConnectionString, queueName);
+
+   Console.WriteLine("Web Job started sending messages in a loop.");
+
+   while (keepRunning)
+   {
+    // Send messages.
+    await SendMessagesAsync(msgCounter);
+    msgCounter++;
+   }
+
+   Console.ReadKey();
+  }
+
+  static async Task SendMessagesAsync(int msgCounter)
+  {
+
+   try
+   {
+
+    // Create a new message to send to the queue
+    string messageBody = $"Message {msgCounter}";
+    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+    // Write the body of the message to the console
+    Console.WriteLine($"Sending message: {messageBody}");
+
+    // Send the message to the queue
+    await queueClient.SendAsync(message);
+
+    Console.WriteLine("Message successfully sent.");
+    Thread.Sleep(10000);
+   }
+   catch (Exception exception)
+   {
+    Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+   }
+
+  }
+  static async void CurrentDomain_ProcessExit(object sender, EventArgs e)
+  {
+   await queueClient.CloseAsync();
+   Console.WriteLine("Exiting sender app");
+  }
+ }
+}
