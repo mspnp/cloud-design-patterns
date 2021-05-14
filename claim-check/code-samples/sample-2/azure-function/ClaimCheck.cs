9140424602200ebf50e4a02322c29edc064815bb
Wenjie Yu <v-wenjyu@microsoft.com>
2021-05-14 11:32:34 +0800

    Upgrade EventGrid to the new SDKs

--- claim-check/code-samples/sample-2/azure-function/ClaimCheck.cs
+++ claim-check/code-samples/sample-2/azure-function/ClaimCheck.cs
@@ -7,8 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Microsoft.Azure.EventGrid;
-using Microsoft.Azure.EventGrid.Models;
+using Azure.Messaging.EventGrid;
+using Azure.Messaging.EventGrid.SystemEvents;
 
 namespace Microsoft.PnP.Messaging
 {
@@ -22,20 +22,18 @@ namespace Microsoft.PnP.Messaging
             string requestContent = await new StreamReader(req.Body).ReadToEndAsync();
             log.LogInformation($"Received events: {requestContent}");
 
-            EventGridSubscriber eventGridSubscriber = new EventGridSubscriber();
-            EventGridEvent[] eventGridEvents = eventGridSubscriber.DeserializeEventGridEvents(requestContent);
+            EventGridEvent[] eventGridEvents = EventGridEvent.ParseMany(BinaryData.FromStream(req.Body));
 
             foreach (EventGridEvent eventGridEvent in eventGridEvents)
             {
-                switch (eventGridEvent.Data) {
+                eventGridEvent.TryGetSystemEventData(out object systemEvent);
+                switch (systemEvent)
+                {
                     case SubscriptionValidationEventData subscriptionValidation:
                         log.LogInformation($"Got SubscriptionValidation event data, validationCode: {subscriptionValidation.ValidationCode},  validationUrl: {subscriptionValidation.ValidationUrl}, topic: {eventGridEvent.Topic}");
                         // Do any additional validation (as required) such as validating that the Azure resource ID of the topic matches
                         // the expected topic and then return back the below response
-                        return new OkObjectResult(new SubscriptionValidationResponse()
-                        {
-                            ValidationResponse = subscriptionValidation.ValidationCode
-                        });
+                        break;
                     case StorageBlobCreatedEventData storageBlobCreated:
                         log.LogInformation($"Got BlobCreated event data, blob URI {storageBlobCreated.Url}");
                         break;
