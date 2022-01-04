250fb99147c660408d35d34cb9985939dac04dce
magrande <mariano.grande@agilesight.com>
2022-01-04 14:23:43 -0300

    updated bindings to match instructions in readme

--- priority-queue/PriorityQueueConsumerHigh/PriorityQueueConsumerHighFn.cs
+++ priority-queue/PriorityQueueConsumerHigh/PriorityQueueConsumerHighFn.cs
@@ -6,7 +6,7 @@ namespace PriorityQueueConsumerHigh
     public static class PriorityQueueConsumerHighFn
     {
         [FunctionName("HighPriorityQueueConsumerFunction")]
-        public static void Run([ServiceBusTrigger("messages", "HighPrioritySubscription", Connection = "ServiceBusConnection")]string highPriorityMessage, ILogger log)
+        public static void Run([ServiceBusTrigger("messages", "highPriority", Connection = "ServiceBusConnection")]string highPriorityMessage, ILogger log)
         {
             log.LogInformation($"C# ServiceBus topic trigger function processed message: {highPriorityMessage}");
         }
