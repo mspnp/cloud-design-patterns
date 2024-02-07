e2d832559823581863a719a8e97663adb1413a1c
Chad Kittel <chad.kittel@gmail.com>
2024-02-07 14:04:07 +0000

    formatting pass

--- pipes-and-filters/ImageProcessingPipeline/Program.cs
+++ pipes-and-filters/ImageProcessingPipeline/Program.cs
@@ -6,8 +6,10 @@ using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices((hostContext, services) => {
-        services.AddAzureClients(c => {
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddAzureClients(c =>
+        {
             c.UseCredential(new DefaultAzureCredential());
             c.AddBlobServiceClient(hostContext.Configuration.GetSection("output")).WithName("processed");
         });
