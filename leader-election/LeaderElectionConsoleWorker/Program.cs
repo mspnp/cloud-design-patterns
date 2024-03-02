2f13c4b023b397672305d9236eb78f264b5cd9f7
Rick Hallihan <1796255+hallihan@users.noreply.github.com>
2024-03-02 14:26:36 -0500

    Update UI and cancellation logic for sample worker

--- leader-election/LeaderElectionConsoleWorker/Program.cs
+++ leader-election/LeaderElectionConsoleWorker/Program.cs
@@ -1,10 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace LeaderElection
+namespace LeaderElectionConsoleWorker
 {
     using System;
     using System.Configuration;
+    using System.Diagnostics;
     using static System.Configuration.ConfigurationManager;
     using System.Threading;
     using System.Threading.Tasks;
@@ -23,7 +24,7 @@ namespace LeaderElection
 
             if (string.IsNullOrEmpty(storageConnStr))
             {
-                Console.WriteLine("A connection string must be set in the app.config file.");
+                Console.Error.WriteLine("A connection string must be set in the app.config file.");
                 return;
             }
             // Create a BlobSettings object with the connection string and the name of the blob to use for the lease
@@ -32,31 +33,37 @@ namespace LeaderElection
                 "leases",
                 "leader");
 
+            // Start an async task that will wait for a keypress and cancel the token when a key is pressed
+            var uiTask = Task.Run(async () =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        Console.ReadKey(true);
+                        Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")}] Requesting shutdown.");
+                        source.Cancel();
+                    }
+                    await Task.Delay(500);
+                }
+                Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")}] This process ({pid}) is shutting down.");
+
+            });
+
             DistributedMutex.BlobDistributedMutex distributedMutex = new DistributedMutex.BlobDistributedMutex(
                 blobSettings,
                 async (CancellationToken token) =>
                 {
-                    var lastUpdate = DateTime.Now.AddSeconds(-16);
                     while (!token.IsCancellationRequested)
                     {
-                        if(DateTime.Now > lastUpdate.AddSeconds(15))
-                        {
-                            Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")}] This process ({pid}) is currently the leader. Press any key to exit.");
-                            lastUpdate = DateTime.Now;
-                        }
-                        // Check for a keypress to stop being the leader
-                        await Task.Delay(1000);
-                        if (Console.KeyAvailable)
-                        {
-                            Console.WriteLine("Stopping this process. Leader position will be released.");
-                            Console.ReadKey(true);
-                            source.Cancel();
-                        }
+                        Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")}] This process ({pid}) is currently the leader. Press any key to exit.");
+                        await Task.Delay(15000);
                     }
                 }, () => {
-                    Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")}] This process ({pid}) could not acquire lease. Retrying in 20 seconds.");
+                    Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")}] This process ({pid}) could not acquire lease. Retrying in 20 seconds. Press any key to exit.");
                 });
             await distributedMutex.RunTaskWhenMutexAcquired(token);
+            await uiTask;
         }
     }
 }
